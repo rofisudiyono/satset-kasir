696816316ee1cf0a503478f600f9e773b5d2f3cb
Rofi Sudiyono <rofisudyono@gmail.com>
2026-05-06 06:23:29 +0700

    ok

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,20 @@
 const { getDefaultConfig } = require("expo/metro-config");
+const {
+  wrapWithReanimatedMetroConfig,
+} = require("react-native-reanimated/metro-config");
 
 const config = getDefaultConfig(__dirname);
+config.resolver.platforms = ["ios", "android", "native", "web"];
+
+config.resolver = {
+  ...config.resolver,
+  unstable_conditionNames: ["browser", "require", "react-native"],
+};
+config.resolver.unstable_enablePackageExports = false;
 
 // Support .mjs files (needed by Tamagui)
 config.resolver.sourceExts.push("mjs");
 
-module.exports = config;
+const reanimatedConfig = wrapWithReanimatedMetroConfig(config);
+
+module.exports = reanimatedConfig;
